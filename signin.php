f266f96a592896d96cec294cd01c7684511ca1c9
loukdev <loukdev@gmail.com>
2015-11-05 17:39:39 +0100

    Suppression fichier dépréciés

--- signin.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("templates/begin.php");
-include_once("templates/header.php");
-include_once("templates/signin_form.php");
-include_once("templates/footer.php");
-include_once("templates/end.php");
-?>
