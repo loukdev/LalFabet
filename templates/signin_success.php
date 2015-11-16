782516f48eef993cb1675c6397d93e65d3228f11
loukdev <loukdev@gmail.com>
2015-11-16 02:24:15 +0100

    Suppression de la proposition de visionner son profil après inscription (invite uniquement à se connecter).

--- templates/signin_success.php
+++ templates/signin_success.php
@@ -1,5 +1,4 @@
 <p>Bravo <?php echo $this->model->cpt_pseudo; ?> ! Vous êtes inscrit à LalFabet !</p>
 <p>
-	Vous pouvez <a href="?connect">vous connecter</a>
-	ou <a href="?user=<?php echo $this->model->cpt_pseudo; ?>">visionner votre profil</a>.
+	Vous pouvez dès à présent vous <a href="?connect">vous connecter</a>.
 </p>
