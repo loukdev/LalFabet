76d2c64a48377cfef211750f2fd31e59ab60df4c
Ordinateur <Ordinateur@192.168.1.12>
2015-12-03 21:43:47 +0100

    Signale désormais que l'abonnement doit être validé lors de la création d'un compte.

--- templates/signin_success.php
+++ templates/signin_success.php
@@ -1,4 +1,4 @@
 <p>Bravo <?php echo $this->model->cpt_pseudo; ?> ! Vous êtes inscrit à LalFabet !</p>
 <p>
-	Vous pouvez dès à présent vous <a href="?connect">vous connecter</a>.
+	Vous devez maintenant attendre qu'un gestionnaire valide votre abonnement.
 </p>
