65cd9f0c2403d785a13be1f27e81ae893405364b
Ordinateur <Ordinateur@192.168.1.12>
2015-12-03 21:30:53 +0100

    Ajout du profil en fonction de l'abonnement.

--- templates/aside.php
+++ templates/aside.php
@@ -2,24 +2,26 @@
 					<ul>
 						<li><a href="?signin">S'inscrire</a></li>
 						<li>
-            <?php if (!isset($_SESSION['cpt_pseudo']))
-                  { 
-                    ?> <a href="?connect">Se connecter</a>
-            <?php } 
-                  else
-                  {
-                    ?><a href="?disconnect">Se déconnecter (<?php echo $_SESSION['cpt_pseudo']; ?>)</a>
-            <?php } ?>
-            </li>
+            <?php	if(!isset($_SESSION['cpt_pseudo']))
+					{ 
+					?>		<a href="?connect">Se connecter</a>
+            <?php	}
+					else
+					{
+					?>		<a href="?disconnect">Se déconnecter (<?php echo $_SESSION['cpt_pseudo']; ?>)</a>
+            <?php	}
+					if(isset($_SESSION['grp_id']))
+					{
+					?>		<a href="?user="<?php echo $_SESSION['cpt_pseudo']; ?>>Profil</a>
+			<?php	}
+
+		?>	</li>
 
             <br />
-<?php
-if (isset($_SESSION['cpt_pseudo']) and $_SESSION['cpt_pseudo'] != '')
-{
-?>
-						<li><a href="">Ajouter une publication</a></li>
-<?php
-}
-?>
+			<?php
+			if (isset($_SESSION['cpt_pseudo']) and $_SESSION['cpt_pseudo'] != '')
+			{
+				?>		<li><a href="">Ajouter une publication</a></li>
+	  <?php } ?>
 					</ul>
 				</aside>
